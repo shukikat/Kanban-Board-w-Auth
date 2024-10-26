4e4db2a7546e157cbfea76d298fa6c918a0252b0
shukikat <kathuriashuki@gmail.com>
2024-10-26 11:15:38 -0400

    added logic to client>src>utils> auth.ts

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -3,16 +3,33 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token=this.getToken(); //needed to define token to access it
+    if (token) {
+    return jwtDecode<JwtPayload>(token); //sends back token as payload
+    }
+    return null; 
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    //only need to get token if user is logged in
     const token=this.getToken();
-    return token;
+    // return token;
+    return !!token && !this.isTokenExpired(token)
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    if(!token) return true;
+    try {
+      const decodedToken=jwtDecode<JwtPayload>(token);
+      const currentTime=Date.now()/1000;
+      return decodedToken.exp < currentTime;
+    } catch (error){
+      console.error('Error decoding token:', error);
+      return true;
+    }
+    
   }
 
   getToken(): string {
