f2d3e10be9a3bbf28a9808e6babdad97be1386dc
shukikat <kathuriashuki@gmail.com>
2024-10-25 22:49:47 -0400

    added initial code to client>src>utils>auth.ts file

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -7,6 +7,8 @@ class AuthService {
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token=this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
@@ -15,14 +17,20 @@ class AuthService {
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser=localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/')
   }
 
   logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
   }
